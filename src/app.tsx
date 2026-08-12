baf5cb9d6325f0875fd7a610ec7ddc9e3c5e280c
SRB Studios <shapubgmobil@gmail.com>
2026-08-12 16:57:29 +0530

    Add full module dashboard layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { GuildSelector } from './pages/GuildSelector';
-import { ModuleDashboard } from './pages/ModuleDashboard';
+import { ModuleDashboard } from './pages/ModuleDashboard'; // 1. Import ModuleDashboard
 
-export const App: React.FC = () => {
+export const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Guild Selection Page */}
-        <Route path="/dashboard" element={<GuildSelector />} />
-
-        {/* Individual Guild Module Dashboard */}
+        <Route path="/servers" element={<GuildSelector />} />
+        
+        {/* 2. Update the path element to point to ModuleDashboard */}
         <Route path="/dashboard/:guildId" element={<ModuleDashboard />} />
-
-        {/* Fallback Route */}
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        
+        <Route path="*" element={<Navigate to="/servers" replace />} />
       </Routes>
     </Router>
   );
